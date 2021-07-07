4a73b08c531e02f3da837ef743060ac92aef34c1
zengzihao <zengzihao@xiaozhu.com>
2021-07-07 16:03:57 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path' 
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  base: './',
+  server: {
+    port: 4000,
+    open: true,
+    cors: true
+  }
+})
